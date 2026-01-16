858fce75fe1f536ad406c5a204d1d61bd105971d
Pablo <pagira@alumnatflorida.es>
2026-01-16 12:00:36 +0100

    añadiendo index

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-require_once 'autoload.php';
-$ClasPresenc = new Presencial ("ProgP" , "Progamacion presencial", 10, "A.2.7");
-var_dump($ClasPresenc);
-?>
+require_once "autoload.php";
+$gestor = new Gestor();
+$progPresen=new Presencial("progP", "Programación Presencial", 10, "A.2.7");
+$progOnline=new Online("progO", "Programación Online", 10, "teams.com");
+var_dump($progPresen);
+var_dump($progOnline);
+
+$gestor->agregar($progPresen);
+$gestor->agregar($progOnline);
+var_dump($_SESSION['asignaturas']);
+
+$gestor->actualizar("progP", "* Programación Presencial *", 1);
+var_dump($_SESSION['asignaturas']);
+
+$encontrado=$gestor->buscar("progO");
+var_dump($encontrado);
+
+$gestor->eliminar("progO");
+var_dump($_SESSION['asignaturas']);
+echo "******************";
